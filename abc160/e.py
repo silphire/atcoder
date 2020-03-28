744978d352c41c82afb8701a7900269e768fa895
Yuki Mitsui <silphire@gmail.com>
2020-03-28 23:13:04 +0900

    smarter solution of E of ABC160

--- abc160/e.py
+++ abc160/e.py
@@ -1,27 +1,7 @@
-import heapq
-
 x, y, a, b, c = map(int, input().split())
-pp = list(map(lambda x: (-int(x), 1), input().split()))
-qq = list(map(lambda x: (-int(x), 2), input().split()))
-rr = list(map(lambda x: (-int(x), 3), input().split()))
+pp = list(sorted(map(int, input().split()), reverse=True))[:x]
+qq = list(sorted(map(int, input().split()), reverse=True))[:y]
+rr = list(map(int, input().split()))
 
-xx = pp + qq + rr
-heapq.heapify(xx)
-t = x + y
-ans = 0
-for _ in range(t):
-    while len(xx) > 0:
-        v = heapq.heappop(xx)
-        if v[1] == 1 and x <= 0:
-            continue
-        if v[1] == 2 and y <= 0:
-            continue
-        break
-
-    if v[1] == 1:
-        x -= 1
-    if v[1] == 2:
-        y -= 1
-    ans += v[0]
-
-print(-ans)
+xx = list(sorted(pp + qq + rr, reverse=True))[:x+y]
+print(sum(xx))
