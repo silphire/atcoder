0484f160d3f81e8c760225713c7167b242591c16
Yuki Mitsui <silphire@gmail.com>
2021-06-28 22:00:22 +0900

    added C of ABL

---
+++ abl/c.py
@@ -0,0 +1,30 @@
+class UnionFind(object):
+    def __init__(self, n: int):
+        self.parent = [x for x in range(n)]
+        self.size = [1] * n
+
+    def root(self, x: int) -> int:
+        if self.parent[x] != x:
+            self.parent[x] = self.root(self.parent[x])
+        return self.parent[x]
+
+    def is_same(self, x: int, y: int) -> bool:
+        return self.root(x) == self.root(y)
+
+    def unite(self, x:int, y: int) -> None:
+        rx = self.root(x)
+        ry = self.root(y)
+        if rx == ry:
+            return
+
+        if self.size[rx] < self.size[ry]:
+            rx, ry = ry, rx
+
+        self.parent[ry] = rx
+        self.size[rx] += self.size[ry]
+
+n, m = map(int, input().split())
+uf = UnionFind(n + 1)
+for _ in range(m):
+    uf.unite(*map(int, input().split()))
+print(len(set(uf.root(x + 1) for x in range(n))) - 1)
