80cd49243cd642724b67f2e8cb4b530f6064dd66
Yuki Mitsui <silphire@gmail.com>
2021-11-30 23:54:17 +0900

    moved J of PAST3 to PAST4

--- past4/j.py
+++ past4/j.py
@@ -3,7 +3,7 @@ class Dijkstra(object):
     * edges: [(weight, vertex_1, vertex_2)]
     """
 
-    def __init__(self, edges, n_vertex: int):
+    def __init__(self, edges, uni_edges, n_vertex: int):
         from collections import defaultdict
 
         assert n_vertex > 0
@@ -17,8 +17,11 @@ class Dijkstra(object):
             w, v1, v2 = e[:3]
             self.route[v1].append((w, v2))
             self.route[v2].append((w, v1))
+        for e in uni_edges:
+            w, v1, v2 = e[:3]
+            self.route[v1].append((w, v2))
 
-    def dijkstra(self, start: int, goal):
+    def dijkstra(self, start: int, goal: int):
         import heapq
 
         assert start < self.n_vertex
@@ -37,7 +40,8 @@ class Dijkstra(object):
                 return w
 
             for wn, vn in self.route[v]:
-                heapq.heappush(q, (w + wn, vn))
+                if not visited[vn]:
+                    heapq.heappush(q, (w + wn, vn))
 
         return float('inf')
 
@@ -46,19 +50,25 @@ n, m = map(int, input().split())
 xab, xac, xbc = map(int, input().split())
 s = input().rstrip()
 ds = {'A': 0, 'B': 1, 'C': 2}
-edges = [
-    (xab, n + 0, n + 1),
-    (xac, n + 0, n + 2),
-    (xbc, n + 1, n + 2),
+edges = []
+uni_edges = [
+    (xab, n + 0, n + 3 + 1),
+    (xac, n + 0, n + 3 + 2),
+    (xbc, n + 1, n + 3 + 2),
+    (xab, n + 3 + 0, n + 1),
+    (xac, n + 3 + 0, n + 2),
+    (xbc, n + 3 + 1, n + 2),
 ]
-for i in range(m):
+for i in range(n):
+    uni_edges.append((0, i, n + 0 + ds[s[i]]))
+    uni_edges.append((0, n + 3 + ds[s[i]], i))
+
+for _ in range(m):
     a, b, c = map(int, input().split())
     a -= 1
     b -= 1
     edges.append((c, a, b))
 
-    edges.append((0, a, n + ds[s[a]]))
-    edges.append((0, b, n + ds[s[b]]))
-dj = Dijkstra(edges, n + 3)
+dj = Dijkstra(edges, uni_edges, n + 3 * 2)
 dist = dj.dijkstra(0, n - 1)
 print(dist)
