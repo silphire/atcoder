fae5257f6f73f94a9ef19bbee65eaa3b27a1cd4e
Yuki Mitsui <silphire@gmail.com>
2022-04-19 23:56:22 +0900

    added E of ABC248

--- abc248/e.py
+++ abc248/e.py
@@ -8,10 +8,23 @@ if k == 1:
     print('Infinity')
     exit()
 
+vis = set()
 ans = 0
 for i in range(n):
-    for j in range(n):
-        for k in range(n):
-            pass
-
+    for j in range(i + 1, n):
+        if (i, j) in vis:
+            continue
+        c = 2
+        s = {i, j}
+        for z in range(j + 1, n):
+            if (xx[j] - xx[i]) * (yy[z] - xx[i]) == (xx[z] - xx[i]) * (yy[j] - yy[i]):
+                c += 1
+                s.add(z)
+        for a in s:
+            for b in s:
+                if a != b:
+                    vis.add((a, b))
+                    vis.add((b, a))
+        if c >= k:
+            ans += 1
 print(ans)
