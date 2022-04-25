1c9003d151ea370bc8918e8c21e84e07ee2337d1
Yuki Mitsui <silphire@gmail.com>
2022-04-25 23:44:44 +0900

    added test for Prime.prime_factorize()

--- lib/lib_test.py
+++ lib/lib_test.py
@@ -9,6 +9,11 @@ class TestPrime(unittest.TestCase):
         self.assertEqual([2], lib.Prime().prime_sequence(2))
         self.assertEqual([2, 3], lib.Prime().prime_sequence(3))
 
+    def test_prime_factorize(self):
+        self.assertCountEqual([], lib.Prime().prime_factorize(1))
+        self.assertCountEqual([2, 2, 3], lib.Prime().prime_factorize(12))
+        self.assertCountEqual([2, 2, 5, 5], lib.Prime().prime_factorize(100))
+
 
 if __name__ == '__main__':
     unittest.main()
