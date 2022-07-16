52376a74707ab973e33a116384fda85a19dcb38c
Yuki Mitsui <silphire@gmail.com>
2022-07-16 23:59:57 +0900

    added E of ABC259

--- abc259/e.py
+++ abc259/e.py
@@ -1,43 +1,13 @@
-from collections import defaultdict
-
 n = int(input())
-mm = [0] * n
-pp = [None] * n
-ee = [None] * n
-aa = defaultdict(list)
+ee = {}
+ii = {}
 for i in range(n):
-    mm[i] = int(input())
-    pp[i] = [0] * mm[i]
-    ee[i] = [0] * mm[i]
-    for j in range(mm[i]):
+    m = int(input())
+    for _ in range(m):
         p, e = map(int, input().split())
-        if p not in aa:
-            aa[p] = [0, 0]
-        if aa[p][0] < e:
-            aa[p][1] = aa[p][0]
-            aa[p][0] = e
-        elif aa[p][1] < e:
-            aa[p][1] = e
-        pp[i][j] = p
-        ee[i][j] = e
-        aa[p].append(e)
-
-ak = sorted(aa.keys())
-s = set()
-for i in range(n):
-    def gen():
-        j = 0
-        for k in ak:
-            if j < mm[i] and pp[i][j] == k:
-                p = pp[i][j]
-                e = ee[i][j]
-                if aa[p][0] == e:
-                    v = aa[p][1]
-                else:
-                    v = aa[p][0]
-                yield (p, v)
-                j += 1
-            else:
-                yield (k, aa[k][0])
-    s.add(tuple((x for x in gen())))
-print(len(s))
+        if p not in ee or ee[p] < e:
+            ee[p], ii[p] = e, i
+        elif ee[p] == e:
+            if p in ii:
+                del ii[p]
+print(min(n, 1 + len(set(ii.values()))))
