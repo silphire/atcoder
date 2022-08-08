5def301d7d09cebf508cbd5682b1fbdc97b3d247
Yuki Mitsui <silphire@gmail.com>
2022-08-08 23:21:19 +0900

    added cumsum with empty list

--- lib/lib_test.py
+++ lib/lib_test.py
@@ -237,6 +237,7 @@ class TestRangeBinaryIndexedTree(unittest.TestCase):
 
 class TestCumsum(unittest.TestCase):
     def test_cumsum(self):
+        self.assertEqual([], lib.cumsum([]))
         self.assertEqual([1, 3, 6, 10], lib.cumsum([1, 2, 3, 4]))
         self.assertEqual([-2, -1, 4, 14], lib.cumsum([-2, 1, 5, 10]))
         self.assertEqual([-1, -1, 0, -1], lib.cumsum([-1, 0, 1, -1]))
