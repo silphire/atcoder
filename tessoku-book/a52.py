51961427905a60c17ccb616c7d291c9c643d6da7
Yuki Mitsui <silphire@gmail.com>
2023-05-05 23:47:58 +0900

    added A52 of tessoku-book

---
+++ tessoku-book/a52.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+que = deque()
+q = int(input())
+for _ in range(q):
+    op, *arg = input().split()
+    op = int(op)
+    if op == 1:
+        que.append(arg[0])
+    elif op == 2:
+        x = que.popleft()
+        que.appendleft(x)
+        print(x)
+    else:
+        que.popleft()
