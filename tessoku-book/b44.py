271bd42d20a3e5be03bf9f280797d687ebdddb2f
Yuki Mitsui <silphire@gmail.com>
2023-07-09 10:30:34 +0900

    added B44 of tessoku-book

---
+++ tessoku-book/b44.py
@@ -0,0 +1,17 @@
+n = int(input())
+aa = [
+    list(map(int, input().split()))
+    for _ in range(n)
+]
+
+tb = [i for i in range(n)]
+q = int(input())
+for _ in range(q):
+    op, y, x = map(int, input().split())
+    x -= 1
+    y -= 1
+    if op == 1:
+        tb[x], tb[y] = tb[y], tb[x]
+    else:
+        print(f"> {tb[y]} {x}")
+        print(aa[tb[y]][x])
