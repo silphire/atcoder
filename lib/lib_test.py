85631104b3d28795f4ad6bba59ffd64361ccc865
Yuki Mitsui <silphire@gmail.com>
2023-09-29 23:42:26 +0900

    added test of bisect_boundary()

--- lib/lib_test.py
+++ lib/lib_test.py
@@ -543,6 +543,7 @@ class TestEditDistance(unittest.TestCase):
 
 class TestBisectBoundary(unittest.TestCase):
     def test_bisect_boundary(self):
+        self.assertRaises(AssertionError, lib.bisect_boundary, list(range(10)), None)
         self.assertEqual(5, lib.bisect_boundary(list(range(10)), lambda x: x <= 5))
 
 
